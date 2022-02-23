d86f206be4ef0b690d241b97631b16483aa50677
Noriaki Mizoguchi <nmizojp@mac.com>
2022-02-23 17:21:47 +0900

    add css

--- JS/test01.js
+++ JS/test01.js
@@ -3,3 +3,20 @@ function test01() {
     console.log(testEl)
     testEl.value = "入力欄の値を変更";
 }
+
+function test02() {
+    const target = document.querySelector("#qtarget");
+    // console.log(target);
+    target.classList.add("preparedClass");
+}
+
+
+function test03() {
+    const target = document.querySelector("#qtarget");
+    target.classList.remove("preparedClass");
+}
+
+function test04() {
+    const target = document.querySelector("#qtarget");
+    target.classList.toggle("preparedClass");
+}
